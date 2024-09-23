c35195509967e7b87b5e4b0c16d395267b47ff8f
Seba Sánchez <89809792+Sebsanchez1602@users.noreply.github.com>
2024-09-23 09:30:04 -0300

    Creación De Proyecto

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+//Aquí se definen las rutas de la aplicación, en este caso solo se tiene una ruta que retorna la vista welcome.
+
+Route::get('/', function () {
+    return view('inicio');
+});
+
+
+Route::get('/Inicio', function () {
+    return view('prueba');
+});  
